b48c1dd386b39978d6fc009789e63fdbd0c53028
shiva krishna <shivakrishna.d16@gmail.com>
2018-08-14 11:05:04 +0530

    without spaces

--- Test/src/TestClass/SingletonExample.java
+++ Test/src/TestClass/SingletonExample.java
@@ -1,21 +1,15 @@
 package TestClass;
 
 public class SingletonExample {
-	public static SingletonExample s=null;
-	
+	public static SingletonExample s = null;
+
 	private SingletonExample() {
-		
-	}
-	
-	public static  SingletonExample  getInstance() {
-		
-		if(s.equals(null)) {
-			s=new SingletonExample();
-		}
-		
-		return s;
-		
-		
 	}
 
+	public static SingletonExample getInstance() {
+		if (s.equals(null)) {
+			s = new SingletonExample();
+		}
+		return s;
+	}
 }
